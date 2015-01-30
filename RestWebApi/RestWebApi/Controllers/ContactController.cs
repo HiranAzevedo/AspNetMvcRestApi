45a98f27faf1cf17f9d10e99ec804bc2d75ff7f4
Hiran <hiran.tassinari@gmail.com>
2015-01-30 00:49:33 -0200

    Commit inicial do projeto e mais um controller que vai ser preenchido com mais rotas para exemplo

---
+++ RestWebApi/RestWebApi/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using RestWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestWebApi.Controllers
+{
+    public class ContactController : ApiController
+    {
+        public Contact[] Get()
+        {
+            return new Contact[] {
+                                new Contact
+                                {
+                                    Id = 1,
+                                    Name = "Glenn Block"
+                                },
+                                new Contact
+                                {
+                                    Id = 2,
+                                    Name = "Dan Roth"
+                                }
+                            };
+        }
+    }
+}
